19faffbd0b72fa424d4f6d230b81a7d5f8f622dd
dekkerl005 <dekkerl005@hva.nl>
2014-11-03 22:36:05 +0100

    FED2
    Update

---
+++ WebApp/static/js/gesture.js
@@ -0,0 +1,20 @@
+var movies = document.getElementById('movies');
+
+// create a simple instance
+// by default, it only adds horizontal recognizers
+var mc = new Hammer(movies);
+
+
+var panFilter = document.getElementById('pan-filter');
+
+// listen to events...
+mc.on("panleft", function(ev) {
+    panFilter.classList.remove('panRight');
+    panFilter.classList.add('panLeft');
+
+});
+
+mc.on("panright", function(ev) {
+    panFilter.classList.remove('panLeft');
+    panFilter.classList.add('panRight');
+});
